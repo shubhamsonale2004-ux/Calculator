911b1149530c2e326c4f69f6cf526496b70bde18
Shubham <shubhamsonale2004@gmail.com>
2026-09-18 19:40:19 +0530

    Use stable Android compile SDK configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.aistudio.calculator.mkvpql"
